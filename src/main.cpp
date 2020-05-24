c28b009cd9e80efb7dc2e64724e13a590a79905b
Christian Jacolot <christian.jacolot@lph.bzh>
2020-05-24 19:01:08 +0200

    Ajout  Wifi en mode AP

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,55 @@
 #include <WiFi.h>
 
 // nom de l’AP sur lequel on va se connecter
-const char* ssid = "nom_AP";
+const char* ssid = "monesp32";
 // mot de passe de l’AP sur lequel on va se connecter
-const char* password = "mot de passe";
+const char* password = "12345678";
 
 // attente en millisecondes
-#define DELAY 1000
+#define DELAY 5000
+
+// fonction d'affichage de la MAC adresse du client
+void printMacAddress(system_event_ap_staconnected_t value) {
+      Serial.print(" avec MAC adresse = ");
+      Serial.print(value.mac[0],HEX);Serial.print(" ");
+      Serial.print(value.mac[1],HEX);Serial.print(" ");
+      Serial.print(value.mac[2],HEX);Serial.print(" ");
+      Serial.print(value.mac[3],HEX);Serial.print(" ");
+      Serial.print(value.mac[4],HEX);Serial.print(" ");
+      Serial.print(value.mac[5],HEX);Serial.print(" ");
+      Serial.print("\n");
+}
+
+// événements 'WiFi'
+void WiFiEvent(WiFiEvent_t event, WiFiEventInfo_t info)
+{
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch (event) {
+    case SYSTEM_EVENT_WIFI_READY:
+      Serial.println("WiFi Ready");
+      break;
+    case SYSTEM_EVENT_AP_START:
+      Serial.println("WiFi access point started");
+      break;
+    case SYSTEM_EVENT_AP_STOP:
+      Serial.println("WiFi access point  stopped");
+      break;
+    case SYSTEM_EVENT_AP_STACONNECTED:
+      Serial.print("Client connected");
+      printMacAddress(info.sta_connected);
+      break;
+    case SYSTEM_EVENT_AP_STADISCONNECTED:
+      Serial.println("Client disconnected");
+      break;
+    case SYSTEM_EVENT_AP_STAIPASSIGNED:
+      Serial.println("Assigned IP address to client");
+      break;
+    case SYSTEM_EVENT_AP_PROBEREQRECVED:
+      Serial.println("Received probe request");
+      break;   
+    default: break;
+  }
+}
 
 // configuration initiale:
 void setup() {
@@ -17,21 +60,16 @@ void setup() {
   delay(1000);
 	Serial.println("\n");
 
-  // connexion au point d'accès
-  WiFi.begin(ssid, password);
-	Serial.print("Connexion en cours...");
-	// attente active de la connexion avec l'AP
-	while(WiFi.status() != WL_CONNECTED)
-	{
-		Serial.print(".");
-		delay(100);
-	}
-	
+  // gestion des événements du WiFi
+  WiFi.onEvent(WiFiEvent);
+  // création du point d'accès
+  Serial.println("Creation du point d'accès...");
+	WiFi.softAP(ssid, password);
+
+  // affichage sur le terminal/monitor de l'adresse IP du point d'accès de l'ESP32
+	Serial.print("Adresse IP de l'AP: ");
+	Serial.println(WiFi.softAPIP());
 	Serial.println("\n");
-	Serial.println("Connexion etablie!");
-	Serial.print("Adresse IP de l'ESP32: ");
-  // affichage sur le terminal de l'adresse IP acquise par l'ESP32 auprès de l'AP
-	Serial.println(WiFi.localIP());
 
   // pour l'ESP 32, LED_BUILTIN vaut 2 (GPIO 2)
   pinMode(LED_BUILTIN, OUTPUT);
@@ -50,4 +88,8 @@ void loop() {
   Serial.println("LED 2 éteinte");
   digitalWrite(LED_BUILTIN, LOW);
   delay(DELAY);
+
+  Serial.print("nombre de clients: ");
+  Serial.print(WiFi.softAPgetStationNum());
+  Serial.println();
 }
