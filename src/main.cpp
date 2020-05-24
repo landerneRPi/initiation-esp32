bc722465eec267df8af341466f2b76921dfd127f
Christian Jacolot <christian.jacolot@lph.bzh>
2020-05-24 17:07:04 +0200

    Code en mode Wifi station

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+// nom de l’AP sur lequel on va se connecter
+const char* ssid = "nom_AP";
+// mot de passe de l’AP sur lequel on va se connecter
+const char* password = "mot de passe";
 
 // attente en millisecondes
 #define DELAY 1000
@@ -7,6 +13,25 @@
 void setup() {
   // liaison série avec le PC (via USB) 
   Serial.begin(115200);
+  // attendre un peu que la connexion se fasse
+  delay(1000);
+	Serial.println("\n");
+
+  // connexion au point d'accès
+  WiFi.begin(ssid, password);
+	Serial.print("Connexion en cours...");
+	// attente active de la connexion avec l'AP
+	while(WiFi.status() != WL_CONNECTED)
+	{
+		Serial.print(".");
+		delay(100);
+	}
+	
+	Serial.println("\n");
+	Serial.println("Connexion etablie!");
+	Serial.print("Adresse IP de l'ESP32: ");
+  // affichage sur le terminal de l'adresse IP acquise par l'ESP32 auprès de l'AP
+	Serial.println(WiFi.localIP());
 
   // pour l'ESP 32, LED_BUILTIN vaut 2 (GPIO 2)
   pinMode(LED_BUILTIN, OUTPUT);
